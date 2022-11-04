24247ceeb5e9aaeaf8808edfd04fbade6503bb18
Essam Ali <essam.3ali@gmail.com>
2022-11-05 01:14:45 +0300

    Add project files.

---
+++ Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Data;
+using Shopping.Client.Models;
+using System.Diagnostics;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        // private readonly HttpClient _httpClient;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IHttpClientFactory httpClientFactory, ILogger<HomeController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            //_httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //var response = await _httpClient.GetAsync("/product");
+            //var content = await response.Content.ReadAsStringAsync();
+            //var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+
+            return View(ProductContext.Products);// productList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
